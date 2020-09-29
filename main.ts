25d7bde71a146490c077ab04a2bc6fa7ca3f1587
kelly102107 <71689279+kelly102107@users.noreply.github.com>
2020-09-29 15:46:56 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,15 @@
-input.onGesture(Gesture.Shake, function () {
-	
-})
 let grade = 0
-if (grade > 80) {
-	
-}
+input.onGesture(Gesture.Shake, function () {
+    grade = randint(80, 100)
+    basic.showNumber(grade)
+    if (grade <= 84) {
+        basic.showString("A-")
+    } else if (grade <= 89) {
+        basic.showString("A")
+    } else if (grade <= 100) {
+        basic.showString("A+")
+    }
+})
 basic.forever(function () {
 	
 })
